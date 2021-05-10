c59ec20ce60c3235cdc6a1bace44bb561ff43553
Mehul Warade <mehulwarade@gmail.com>
2021-05-10 11:30:15 +1000

    first

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4000
+
+app.listen(port, () => {
+  console.log(`http server port: ${port}`);
+});
+
+const base = `${__dirname}/public`;
+
+// Since .css files are static files you have to serve them to the clients. 
+// However, you do not serve static files as a express middleware. 
+// Add the following middleware to your express app and move the css folder under the public directory 
+// (you should create a public directory)
+
+app.use(express.static('public'));  
+
+
+app.get('/', function(req, res) {
+  res.sendFile(`${base}/login.html`);
+});
+
+app.get('/test', function(req, res) {
+  res.sendFile(`${base}/test.html`);
+});
